d21992b99e458c6605cb7e45dc50212174ebbc50
joaovict18 <jovimedim@gmail.com>
2024-06-25 18:10:02 -0300

    fazendo versão difícil do exercício do mario

---
+++ cc50-exercises/c-exercises/mario-hard/mario.c
@@ -0,0 +1,45 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void printPyramid(int height);
+
+int main(void) { // prototype
+
+    int height;
+
+    do {
+        int height = get_int("Height: ");
+    } while (height < 1 || height >= 8);
+
+    if (height == 1) {
+        printf("# #\n");
+    }
+    else {
+        printPyramid(height);
+    }
+}
+
+void printPyramid(int height) { // function that holds the role of build the pyramid
+
+    for (int i = 0; i < height; i++) {
+
+        for (int j = height - 1; j > i; j--) {
+            printf(" ");
+        }
+
+        for (int k = 0; k < i; k++) {
+            printf("#");
+        }
+
+        printf(" ");
+
+        for (int l = 0; l < i; l++) {
+            printf("#");
+        }
+
+        for (int m = height; m > i; m--) {
+            printf(" ");
+        }
+        printf("\n"); //! cat's jump (that makes more sense in portuguese)
+    }
+}
